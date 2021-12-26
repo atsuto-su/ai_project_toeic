b6e38be17426cc880c4994be51f66e311fb7a785
atsuto-su <astm.develop.70@gmail.com>
2021-12-26 13:50:19 +0900

    updated to generate ocr texts file into intermediate folder.

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ import os
 from set_credentials import set_credentials
 from toeic_text_converter import ToeicTextConverter
 
-
 # folder setting
 input_folder = "input/QuestionSound"
+intermed_folder = "intermediate"
 output_folder = "output"
-
-
 
 # set authentification key for Google API
 set_credentials()
@@ -17,11 +15,12 @@ toeic_converter = ToeicTextConverter()
 
 # execute Part1 data
 # インプットフォルダ・アウトプットフォルダを指定するだけ。
-input_part1 = "input/QuestionSound/Part1"
-output_part1 = "output/Part1"
+part1_folder = "Part1"
+img_file = "01.jpg"
 
-input_file = input_part1 + "/" + "10.jpg"
-output_file = output_part1 + "/" + os.path.basename(input_file).replace(".jpg", ".mp4")
+input_file = "/".join([input_folder, part1_folder, img_file])
+intermed_file = "/".join([intermed_folder, part1_folder, os.path.basename(input_file).replace(".jpg", ".txt")])
+output_file = "/".join([output_folder, part1_folder, os.path.basename(input_file).replace(".jpg", ".mp4")])
 
 toeic_conv = ToeicTextConverter()
-toeic_conv.part1_conv(input_file, output_file)
+toeic_conv.part1_conv(input_file, output_file, intermed_file)
--- toeic_text_converter.py
+++ toeic_text_converter.py
@@ -6,16 +6,16 @@ class ToeicTextConverter():
     def __init__(self):
         pass
 
-    def part1_conv(self, img_file, out_file):
+    def part1_conv(self, img_file, out_file, intermed_file=""):
         #input: image file
         #output: speech text (single speaker)
         img_text = exec_my_ocr.exec_ocr(img_file)
+        if intermed_file:
+            self.save_ocr_strings(img_text, intermed_file)
         exec_my_tts.single_speaker_tts(img_text, out_file)
-        pass
 
     def part2_conv(self, img_file, out_file):
         self.part1_conv(img_file, out_file)
-        pass
 
     def part3_conv(self, img_file, out_file):
         pass
@@ -23,3 +23,6 @@ class ToeicTextConverter():
     def part4_conv(self, img_file, out_file):
         pass
    
+    def save_ocr_strings(self, ocr_string, file_name) -> None:
+        with open(file_name, 'wt', encoding='utf-8') as f:
+            f.write(ocr_string)
